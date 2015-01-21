aa28da92b9dd22dfe8e571f4966df053d28f6155
Lukas Heinrich <lukas.heinrich@cern.ch>
2015-01-21 22:13:04 +0100

    restructured analyses

--- catalogue.py
+++ catalogue.py
@@ -1,17 +1,17 @@
-import dmhiggs_analysis.dmhiggs_backendtasks
-import dmhiggs_analysis.dmhiggs_blueprint
-import hype_analysis.hype_backendtasks
-import hype_analysis.hype_blueprint
+import dmhiggs_backendtasks
+import dmhiggs_blueprint
+import hype_backendtasks
+import hype_blueprint
 
 implemented_analyses = {
-  dmhiggs_analysis.dmhiggs_blueprint.RECAST_ANALYSIS_ID : 
+  dmhiggs_blueprint.RECAST_ANALYSIS_ID : 
       {
-       'workflow':dmhiggs_analysis.dmhiggs_backendtasks,
-       'blueprint': dmhiggs_analysis.dmhiggs_blueprint.blueprint
+       'workflow':dmhiggs_backendtasks,
+       'blueprint':dmhiggs_blueprint.blueprint
        },
-  hype_analysis.hype_blueprint.RECAST_ANALYSIS_ID : 
+  hype_blueprint.RECAST_ANALYSIS_ID : 
       {
-       'workflow':hype_analysis.hype_backendtasks,
-       'blueprint':hype_analysis.hype_blueprint.blueprint
+       'workflow':hype_backendtasks,
+       'blueprint':hype_blueprint.blueprint
       }
 }
