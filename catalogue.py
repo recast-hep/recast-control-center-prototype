ed4f33a5fb6ef4b8c31784c39839a8280ab1916e
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-01-23 16:24:52 +0100

    setuptools

--- catalogue.py
+++ catalogue.py
@@ -1,18 +1,17 @@
-# import dmhiggs_backendtasks
-# import dmhiggs_blueprint
-# import hype_backendtasks
-# import hype_blueprint
-#
-implemented_analyses = {}
-#implemented_analyses = {
-#   dmhiggs_blueprint.RECAST_ANALYSIS_ID :
-#       {
-#        'workflow':dmhiggs_backendtasks,
-#        'blueprint':dmhiggs_blueprint.blueprint
-#        },
-#   hype_blueprint.RECAST_ANALYSIS_ID :
-#       {
-#        'workflow':hype_backendtasks,
-#        'blueprint':hype_blueprint.blueprint
-#       }
-# }
+import recastdmhiggs.dmhiggs_backendtasks as dmhiggs_backendtasks
+import recastdmhiggs.dmhiggs_blueprint as dmhiggs_blueprint
+import recasthype.hype_backendtasks as hype_backendtasks
+import recasthype.hype_blueprint as hype_blueprint
+
+implemented_analyses = {
+  dmhiggs_blueprint.RECAST_ANALYSIS_ID :
+      {
+       'workflow':dmhiggs_backendtasks,
+       'blueprint':dmhiggs_blueprint.blueprint
+       },
+  hype_blueprint.RECAST_ANALYSIS_ID :
+      {
+       'workflow':hype_backendtasks,
+       'blueprint':hype_blueprint.blueprint
+      }
+}
--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
-
-
 setup(
   name = 'recast-frontend-prototype',
   version = '0.0.1',
@@ -18,16 +16,16 @@ setup(
     'gevent-socketio',
     'msgpack-python',
     'socket.io-python-emitter',
-    # 'recast-dmhiggs-demo',
-    # 'recast-rivet-recaster-demo',
-    # 'recast-hype-demo',
+    'recast-dmhiggs-demo',
+    'recast-rivet-recaster-demo',
+    'recast-hype-demo',
     'recast-api'
   ],
   dependency_links = [
     'https://github.com/ziyasal/socket.io-python-emitter/tarball/master#egg=socket.io-python-emitter-0.1.3',
-    # 'http://github.com/lukasheinrich/recast-dmhiggs-demo/tarball/master#egg=recast-dmhiggs-demo',
-    # 'http://github.com/lukasheinrich/recast-rivet-recaster-demo/tarball/master#egg=recast-rivet-recaster-demo',
-    # 'http://github.com/lukasheinrich/recast-hype-demo/tarball/master#egg=recast-hype-demo',
+    'http://github.com/lukasheinrich/recast-dmhiggs-demo/tarball/master#egg=recast-dmhiggs-demo-0.0.1',
+    'http://github.com/lukasheinrich/recast-rivet-recaster-demo/tarball/master#egg=recast-rivet-recaster-demo-0.0.1',
+    'http://github.com/lukasheinrich/recast-hype-demo/tarball/master#egg=recast-hype-demo-0.0.1',
     'http://github.com/cranmer/recast-api/tarball/master#egg=recast-api-0.0.1'
   ]
 )
