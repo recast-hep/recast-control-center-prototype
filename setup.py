814271e2d85cf27875b9c3b8a23c726af43b21f3
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-02-01 18:19:43 -0500

    move to recast-hep

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   version = '0.0.1',
   description = 'prototype web frontend for RECAST project at CERN',
   long_description = 'prototype web frontend for RECAST project at CERN. Provides users with options to request new RECAST requests, process them using a backend, and display results.',
-  url = 'http://github.com/lukasheinrich/recast-frontend-prototype',
+  url = 'http://github.com/recast-hep/recast-frontend-prototype',
   author = 'Lukas Heinrich',
   author_email = 'lukas.heinrich@cern.ch',
   packages=find_packages(),
@@ -22,10 +22,10 @@ setup(
     'recast-api'
   ],
   dependency_links = [
-    'http://github.com/ziyasal/socket.io-python-emitter/tarball/master#egg=socket.io-python-emitter-0.1.3',
-    'http://github.com/lukasheinrich/recast-dmhiggs-demo/tarball/master#egg=recast-dmhiggs-demo-0.0.1',
-    'http://github.com/lukasheinrich/recast-rivet-recaster-demo/tarball/master#egg=recast-rivet-recaster-demo-0.0.1',
-    'http://github.com/lukasheinrich/recast-hype-demo/tarball/master#egg=recast-hype-demo-0.0.1',
-    'http://github.com/cranmer/recast-api/tarball/master#egg=recast-api-0.0.1'
+    'https://github.com/ziyasal/socket.io-python-emitter/tarball/master#egg=socket.io-python-emitter-0.1.3',
+    'https://github.com/recast-hep/recast-dmhiggs-demo/tarball/master#egg=recast-dmhiggs-demo-0.0.1',
+    'https://github.com/recast-hep/recast-rivet-recaster-demo/tarball/master#egg=recast-rivet-recaster-demo-0.0.1',
+    'https://github.com/recast-hep/recast-hype-demo/tarball/master#egg=recast-hype-demo-0.0.1',
+    'https://github.com/recast-hep/recast-api/tarball/master#egg=recast-api-0.0.1'
   ]
 )
