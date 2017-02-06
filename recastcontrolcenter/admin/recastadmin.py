de87c5e1254bc1d65ea3e3c6472f749517743d72
lukasheinrich <lukas.heinrich@gmail.com>
2017-02-06 14:27:24 +0100

    cleaning up

--- recastcontrolcenter/admin/recastadmin.py
+++ recastcontrolcenter/admin/recastadmin.py
@@ -1,24 +1,28 @@
-#needed to remove Exception KeyError: KeyError(4384790800,) in <module 'threading' ... > error
-from gevent import monkey; monkey.patch_all() 
+# needed to remove Exception KeyError: KeyError(4384790800,) in <module
+# 'threading' ... > error
+from gevent import monkey
+monkey.patch_all()
 
 from recastdb.models import db
 from recastcontrolcenter.server import create_app
 import click
 import IPython
 
+
 @click.group()
 def recastadmin():
-  pass
+    pass
+
 
 @recastadmin.command()
-@click.option('--config',default = None)
+@click.option('--config', default=None)
 def create_db(config):
-  db.create_all(app = create_app(config))
+    db.create_all(app=create_app(config))
+
 
 @recastadmin.command()
 def db_shell():
-  import recastdb.models as models
-  with create_app().app_context():
-    IPython.embed()
-  
-  
+    import recastdb.models as models
+    print dir(models)
+    with create_app().app_context():
+        IPython.embed()
