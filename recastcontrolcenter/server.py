c92d126f283e4448d876f802fead384b128f37ea
lukasheinrich <lukas.heinrich@gmail.com>
2017-10-15 23:19:04 -0500

    change back ATLAS group

--- recastcontrolcenter/server.py
+++ recastcontrolcenter/server.py
@@ -89,7 +89,7 @@ def extract_user_info(userdata):
     egroup_to_expt = {
         'cms-members': 'CMS',
         'alice-member': 'ALICE',
-        'atlas-active-physicists': 'ATLAS',
+        'atlas-current-physicists': 'ATLAS',
         'lhcb-general': 'LHCb'
     }
 
